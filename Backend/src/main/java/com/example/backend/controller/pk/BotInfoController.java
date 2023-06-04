750592967765dda67d9dba0baa06c0353b0d9fb2
zhengtianl <458078290@qq.com>
2023-06-04 14:02:30 +1000

    'changeTheWholeProject'

--- Backend/src/main/java/com/example/backend/controller/pk/BotInfoController.java
+++ Backend/src/main/java/com/example/backend/controller/pk/BotInfoController.java
@@ -2,12 +2,17 @@ package com.example.backend.controller.pk;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public String getBotInfo(){
-        return "hhhhh";
+    public Map<String, Integer> getBotInfo(){
+        Map<String, Integer> botInfo = new HashMap<>();
+        botInfo.put("name", 5);
+        botInfo.put("rating", 10000);
+        return botInfo;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <div>bot name:{{bot_name}}</div>
+    <div> bot number:{{bot_rating}}</div>
+  </div>
   <router-view/>
 </template>
 
+<script>
+import { ref } from "vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+    
+    axios.get("http://localhost:3000/pk/getbotinfo/")
+      .then((response) => {
+        console.log(response.data);
+        bot_name.value = response.data.name;
+        bot_rating.value = response.data.rating;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+
+    return {
+      bot_name,
+      bot_rating,
+    };
+  },
+};
+
+
+
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
